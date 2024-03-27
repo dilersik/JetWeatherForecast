4c574d08b97f8eb44f3127872eddeb66ac6cc436
Dilermando Augusto <di_ler@hotmail.com>
2024-03-27 14:11:24 -0300

    178. Setting up the ViewModel and Retrieving JSON Payload

--- app/src/main/java/com/example/jetweatherforecast/ui/views/main/MainView.kt
+++ app/src/main/java/com/example/jetweatherforecast/ui/views/main/MainView.kt
@@ -1,10 +1,30 @@
 package com.example.jetweatherforecast.ui.views.main
 
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.platform.LocalContext
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
+import com.example.jetweatherforecast.util.showToast
 
 @Composable
 fun MainView(navController: NavController) {
-    Text(text = "MAIN")
+    val mainViewModel: MainViewModel = hiltViewModel()
+    ShowForecast(mainViewModel)
+}
+
+@Composable
+fun ShowForecast(mainViewModel: MainViewModel) {
+    val context = LocalContext.current
+    val forecast = mainViewModel.forecast.collectAsState().value
+    val error = mainViewModel.error.collectAsState().value
+
+    when {
+        mainViewModel.loading.collectAsState().value -> CircularProgressIndicator()
+        error != null -> context.showToast(error)
+        forecast != null -> Text(text = "MAIN ${forecast.city.country}")
+    }
+
 }
--- app/src/main/java/com/example/jetweatherforecast/ui/views/main/MainViewModel.kt
+++ app/src/main/java/com/example/jetweatherforecast/ui/views/main/MainViewModel.kt
@@ -1,7 +1,5 @@
 package com.example.jetweatherforecast.ui.views.main
 
-import androidx.compose.runtime.MutableState
-import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.example.jetweatherforecast.model.Forecast
@@ -22,9 +20,10 @@ class MainViewModel @Inject constructor(
     val loading = _loading.asStateFlow()
 
     private val _error: MutableStateFlow<String?> = MutableStateFlow(null)
-    val error = _loading.asStateFlow()
+    val error = _error.asStateFlow()
 
-    private val forecast: MutableState<Forecast?> = mutableStateOf(null)
+    private val _forecast: MutableStateFlow<Forecast?> = MutableStateFlow(null)
+    val forecast = _forecast.asStateFlow()
 
     init {
         loadForecast()
@@ -34,7 +33,7 @@ class MainViewModel @Inject constructor(
         if (city.isEmpty()) return@launch
         _loading.value = true
         when (val result = weatherRepository.getForecast(city)) {
-            is ResultWrapper.Success -> forecast.value = result.data
+            is ResultWrapper.Success -> _forecast.value = result.data
             is ResultWrapper.Error -> _error.value = result.exception.message
         }
         _loading.value = false
