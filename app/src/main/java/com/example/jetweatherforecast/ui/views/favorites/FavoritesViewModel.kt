d39ba4609e97b66d0cdf245df1bdb70599466501
Dilermando Augusto <di_ler@hotmail.com>
2024-04-02 13:52:44 -0300

    196. Creating the FavoriteViewModel Class

---
+++ app/src/main/java/com/example/jetweatherforecast/ui/views/favorites/FavoritesViewModel.kt
@@ -0,0 +1,47 @@
+package com.example.jetweatherforecast.ui.views.favorites
+
+import android.util.Log
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.jetweatherforecast.model.local.Favorite
+import com.example.jetweatherforecast.repository.local.FavoriteLocalRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class FavoritesViewModel @Inject constructor(
+    private val favoriteLocalRepository: FavoriteLocalRepository
+) : ViewModel() {
+
+    private val _favorites = MutableStateFlow<List<Favorite>>(emptyList())
+    val favorites = _favorites.asStateFlow()
+
+    init {
+        viewModelScope.launch(Dispatchers.IO) {
+            try {
+                favoriteLocalRepository.getAll().distinctUntilChanged().collect {
+                    _favorites.value = it
+                }
+            } catch (e: Exception) {
+                Log.d(TAG, e.message.toString())
+            }
+        }
+    }
+
+    fun addToFavorite(favorite: Favorite) = viewModelScope.launch {
+        try {
+            favoriteLocalRepository.insert(favorite)
+        } catch (e: Exception) {
+            Log.d(TAG, e.message.toString())
+        }
+    }
+
+    private companion object {
+        private const val TAG = "FavoritesViewModel"
+    }
+}
